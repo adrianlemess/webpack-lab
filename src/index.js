4e3f1ca16a13e50994b74987ea14a76f195233a7
Adrian Lemes Caetano <adrianlemescaetano@MacBook-Pro-de-Adrian.local>
2019-03-27 23:51:50 -0300

    add loaders in webpack

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import './style.css';
+// import Img from './images.jpg';
+
+function component() {
+    let element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // let img = new Image();
+    // img.src = Img;
+
+    // element.appendChild(img)
+    return element;
+  }
+  
+  document.body.appendChild(component());
