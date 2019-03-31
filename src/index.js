331d4cc19e281afad2fc311bbe96ba081d191e0b
Adrian Lemes Caetano <adrianlemescaetano@MacBook-Pro-de-Adrian.local>
2019-03-31 20:42:04 -0300

    remove files to prepare for the next section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import _ from 'lodash';
-import './style.css';
-// import Img from './images.jpg';
-import Data from './data.xml';
 
 function component() {
-    console.log(Data)
     let element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
